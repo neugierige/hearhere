1a2cd4b26818a638b8d227089bec43513852cd04
matt-korp <matt@korp.me>
2015-03-05 19:31:36 -0500

    sign up VC's, config added

---
+++ HearHereApp/DataManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataManager.swift
+//  HearHereApp
+//
+//  Created by Matthew Korporaal on 3/5/15.
+//  Copyright (c) 2015 LXing. All rights reserved.
+//
+
+import Foundation
---
+++ HearHereApp/HearHereApp/Configuration.swift
@@ -0,0 +1,17 @@
+//
+//  Configuration.swift
+//  HearHereApp
+//
+//  Created by Matthew Korporaal on 3/5/15.
+//  Copyright (c) 2015 LXing. All rights reserved.
+//
+
+import UIKit
+
+struct Configuration {
+    
+    static var backgroundUIColor = UIColor(red:0.38, green:0.38, blue:0.38, alpha:1)
+    static var userSession: PFUser!
+    static var applicationID = "mTDskuO4PaaHfOKdqA2uvPyx8HqozJx8OUT9a3cL"
+    static var clientKey = "j4fFLe93tqZ2A5ye14MxlWh7Ovnh0L6QP6WmP2Yg"
+}
